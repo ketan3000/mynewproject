bffd7b322601cf8d3dd199ce73c4e7a631cc39a4
kkc <kkc1909@gmail.com>
2018-08-07 21:45:05 +0530

    u2

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+1.Create a repositories in github or bigbucket and create a project in your repositories 
+example : https://github.com/ketan3000/mynewproject.git
+
+2. Clone the file in the local system:
+command : git clone https://github.com/ketan3000/mynewproject.git
+
+
+if branch you are created then run below command: 
+
+3: Check the branch
+
+command : git branch
+
+4: Change the branch:
+
+git checkout branch_name
+
+
+
+
+
 <?php 
 
 echo "hello world world 2 sradha raj";
+
