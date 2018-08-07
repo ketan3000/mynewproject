af8e6e6485e592b6742cba4b5b306fa1bf1bab62
kkc <kkc1909@gmail.com>
2018-08-07 21:55:21 +0530

    sradha update

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ command : git clone https://github.com/ketan3000/mynewproject.git
 
 
 if branch you are created then run below command: 
+git clone -b sradha https://github.com/ketan3000/mynewproject.git
 
 3: Check the branch
 
@@ -14,6 +15,23 @@ command : git branch
 4: Change the branch:
 
 git checkout branch_name
+
+5: git status //// check the status 
+
+5: git add . ////// adds / stages all of the files in the current directory
+
+6: git commit -m "commit"
+
+7: git push or git push origin branch_name // push the code in the repositories
+
+===============================================================================
+
+How to pull file from the github server:
+
+git pull origin branch_name
+
+
+
 
 
 
